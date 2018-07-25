786ad41ebf2324a87b2143f254787999fede1933
devenlf <1821415702@qq.com>
2018-07-25 15:09:03 +0800

    保存

--- src/api/user.js
+++ src/api/user.js
@@ -1,16 +1,13 @@
 import service from '@/utils/request'
 //相关接口调用
 export function login(account,password) {
-    const data = {
-      account: account,
-      password: password
-    }
     return service({
       url: 'login2',
       method: 'POST',
       headers:{
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
       },
-      data
+      data:'account='+account+'&password='+password+''
     })
   }
